bc8713e7279b075ba1f3be5f1904043d764f0711
castellarintobias@gmail.com <castellarintobias@gmail.com>
2024-12-01 15:51:38 -0300

    remote build

--- rxconfig.py
+++ rxconfig.py
@@ -2,5 +2,9 @@ import reflex as rx
 
 config = rx.Config(
     app_name="Negocio",
-    api_url=""
+    api_url="https://castellarinautorepuestos.up.railway.app/",
+    cors_allowed_origins=[
+        "https://localhost:3000/",
+        "https://castellarinautorepuestos.vercel.app/"
+    ]
 )
