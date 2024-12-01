183b405f7e895cc63e4b376aaa906e20674f4dfa
castellarintobias@gmail.com <castellarintobias@gmail.com>
2024-12-01 16:17:22 -0300

    prueba de trigger

--- rxconfig.py
+++ rxconfig.py
@@ -4,7 +4,6 @@ config = rx.Config(
     app_name="Negocio",
     api_url="https://castellarinautorepuestos.up.railway.app/",
     cors_allowed_origins=[
-        "https://localhost:3000/",
         "https://castellarinautorepuestos.vercel.app/"
     ]
 )
