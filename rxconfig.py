6f0a2495ae9ba6d351cfd3e4e49ec7cc07d3acbc
castellarintobias@gmail.com <castellarintobias@gmail.com>
2024-12-06 22:45:09 -0300

    Cambios en el CORS

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="Negocio",
-    cors_allowed_origins=["*"]
-
+    cors_allowed_origins=["https://castellarinautorepuestos.vercel.app"],
+    allow_websocket_origins=["https://castellarinautorepuestos.vercel.app"]
 )
+
 
